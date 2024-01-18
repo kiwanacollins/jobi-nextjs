034071d556fd276d14e22ed8409a14aef64e6212
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-19 04:11:03 +0600

    Update webhook secret variable in route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
 
   // Todo: Add your webhook secret to .env.local
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -60,7 +60,6 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name } = evt.data;
 
     // Todo: create a user in your database
-
     const mongoUser = await createUser({
       clerkId: id,
       name: `${first_name}${last_name ? ` ${last_name}` : ''}`,
