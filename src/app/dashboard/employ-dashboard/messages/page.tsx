259b26f59ba5b4d716847ccb28b585fab2af77ba
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 04:15:03 +0600

    Refactor employ dashboard messages page

--- src/app/dashboard/employ-dashboard/messages/page.tsx
+++ src/app/dashboard/employ-dashboard/messages/page.tsx
@@ -1,24 +1,13 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from '@/layouts/wrapper';
-import EmployAside from '@/app/components/dashboard/employ/aside';
+import React from 'react';
 import DashboardMessage from '@/app/components/dashboard/candidate/dashboard-message';
 
 const EmployDashboardMessagesPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-
-    <div className='main-page-wrapper'>
-      {/* aside start */}
-      <EmployAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar}/>
-      {/* aside end  */}
-
+    <div>
       {/* messages area start */}
-      <DashboardMessage setIsOpenSidebar={setIsOpenSidebar}/>
+      <DashboardMessage />
       {/* messages area end */}
     </div>
-    </Wrapper>
   );
 };
 
