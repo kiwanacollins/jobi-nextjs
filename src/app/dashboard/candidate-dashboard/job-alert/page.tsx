e5c91ec2fbeefdf1c1ab8bbf0c2381f2cfe60be5
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 03:03:55 +0600

    Refactor job alert and setting pages

--- src/app/dashboard/candidate-dashboard/job-alert/page.tsx
+++ src/app/dashboard/candidate-dashboard/job-alert/page.tsx
@@ -1,23 +1,13 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from "@/layouts/wrapper";
-import CandidateAside from "@/app/components/dashboard/candidate/aside";
-import JobAlertArea from "@/app/components/dashboard/candidate/job-alert-area";
+import React from 'react';
+import JobAlertArea from '@/app/components/dashboard/candidate/job-alert-area';
 
 const CandidateProfileJobAlertPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-      <div className="main-page-wrapper">
-        {/* aside start */}
-        <CandidateAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar}/>
-        {/* aside end  */}
-
-        {/* job alert area start */}
-        <JobAlertArea setIsOpenSidebar={setIsOpenSidebar} />
-        {/* job alert area end */}
-      </div>
-    </Wrapper>
+    <div>
+      {/* job alert area start */}
+      <JobAlertArea />
+      {/* job alert area end */}
+    </div>
   );
 };
 
--- src/app/dashboard/candidate-dashboard/setting/page.tsx
+++ src/app/dashboard/candidate-dashboard/setting/page.tsx
@@ -1,23 +1,14 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from "@/layouts/wrapper";
-import CandidateAside from "@/app/components/dashboard/candidate/aside";
-import DashboardSettingArea from "@/app/components/dashboard/candidate/dashboard-setting";
+import React from 'react';
+
+import DashboardSettingArea from '@/app/components/dashboard/candidate/dashboard-setting';
 
 const CandidateDashboardSettingPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-      <div className="main-page-wrapper">
-        {/* aside start */}
-        <CandidateAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar} />
-        {/* aside end  */}
-
-        {/* setting area start */}
-        <DashboardSettingArea setIsOpenSidebar={setIsOpenSidebar} />
-        {/* setting area end */}
-      </div>
-    </Wrapper>
+    <div className="main-page-wrapper">
+      {/* setting area start */}
+      <DashboardSettingArea />
+      {/* setting area end */}
+    </div>
   );
 };
 
