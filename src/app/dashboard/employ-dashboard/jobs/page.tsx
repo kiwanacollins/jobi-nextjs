d3826d233228c8c1a00f89ebb4d51503a87d65c2
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 04:13:11 +0600

    Refactor employ-dashboard profile and jobs pages

--- src/app/dashboard/employ-dashboard/jobs/page.tsx
+++ src/app/dashboard/employ-dashboard/jobs/page.tsx
@@ -1,24 +1,14 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from '@/layouts/wrapper';
-import EmployAside from '@/app/components/dashboard/employ/aside';
+import React from 'react';
+
 import EmployJobArea from '@/app/components/dashboard/employ/job-area';
 
 const EmployDashboardJobsPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-
-    <div className='main-page-wrapper'>
-      {/* aside start */}
-      <EmployAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar}/>
-      {/* aside end  */}
-
+    <>
       {/* job area start */}
-      <EmployJobArea setIsOpenSidebar={setIsOpenSidebar}/>
+      <EmployJobArea />
       {/* job area end */}
-    </div>
-    </Wrapper>
+    </>
   );
 };
 
--- src/app/dashboard/employ-dashboard/profile/page.tsx
+++ src/app/dashboard/employ-dashboard/profile/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-import React from 'react';
 import EmployProfileArea from '@/app/components/dashboard/employ/profile-area';
 
 const EmployDashboardProfilePage = () => {
