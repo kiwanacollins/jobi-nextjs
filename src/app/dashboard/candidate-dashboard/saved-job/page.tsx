3767708168e89fa5da5c87b01dfe3d4c088733ae
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 02:43:48 +0600

    Refactor SavedJobArea component

--- src/app/dashboard/candidate-dashboard/saved-job/page.tsx
+++ src/app/dashboard/candidate-dashboard/saved-job/page.tsx
@@ -1,23 +1,13 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from "@/layouts/wrapper";
-import CandidateAside from "@/app/components/dashboard/candidate/aside";
-import SavedJobArea from "@/app/components/dashboard/candidate/saved-job-area";
+import React from 'react';
+import SavedJobArea from '@/app/components/dashboard/candidate/saved-job-area';
 
 const CandidateDashboardSavedJobPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-      <div className="main-page-wrapper">
-        {/* aside start */}
-        <CandidateAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar} />
-        {/* aside end  */}
-
-        {/* saved job area start */}
-        <SavedJobArea setIsOpenSidebar={setIsOpenSidebar} />
-        {/* saved job area end */}
-      </div>
-    </Wrapper>
+    <>
+      {/* saved job area start */}
+      <SavedJobArea />
+      {/* saved job area end */}
+    </>
   );
 };
 
