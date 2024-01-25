82d500bf0a73f779062ee88615ce7fe6f1f3861e
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 04:37:32 +0600

    Refactor employ dashboard setting page

--- src/app/dashboard/employ-dashboard/setting/page.tsx
+++ src/app/dashboard/employ-dashboard/setting/page.tsx
@@ -1,24 +1,13 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from '@/layouts/wrapper';
-import EmployAside from '@/app/components/dashboard/employ/aside';
+import React from 'react';
 import DashboardSettingArea from '@/app/components/dashboard/candidate/dashboard-setting';
 
 const EmployDashboardSettingPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-
-    <div className='main-page-wrapper'>
-      {/* aside start */}
-      <EmployAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar}/>
-      {/* aside end  */}
-
+    <>
       {/* dashboard area start */}
-      <DashboardSettingArea setIsOpenSidebar={setIsOpenSidebar}/>
+      <DashboardSettingArea />
       {/* dashboard area end */}
-    </div>
-    </Wrapper>
+    </>
   );
 };
 
