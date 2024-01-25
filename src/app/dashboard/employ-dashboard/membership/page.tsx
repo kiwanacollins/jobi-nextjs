979cfd841fafe56053d7cc45cc77ce18b124b8cf
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 04:36:05 +0600

    Refactor employ membership area component

--- src/app/dashboard/employ-dashboard/membership/page.tsx
+++ src/app/dashboard/employ-dashboard/membership/page.tsx
@@ -1,23 +1,13 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from "@/layouts/wrapper";
-import EmployAside from "@/app/components/dashboard/employ/aside";
-import EmployMembershipArea from "@/app/components/dashboard/employ/membership-area";
+import React from 'react';
+import EmployMembershipArea from '@/app/components/dashboard/employ/membership-area';
 
 const EmployDashboardMembershipPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-      <div className="main-page-wrapper">
-        {/* aside start */}
-        <EmployAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar} />
-        {/* aside end  */}
-
-        {/* membership area start */}
-       <EmployMembershipArea setIsOpenSidebar={setIsOpenSidebar}/>
-        {/* membership area end */}
-      </div>
-    </Wrapper>
+    <>
+      {/* membership area start */}
+      <EmployMembershipArea />
+      {/* membership area end */}
+    </>
   );
 };
 
