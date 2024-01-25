269dc568ec62e5d18310f26b03a03402f500de4a
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-26 02:52:19 +0600

    Refactor candidate dashboard messages page

--- src/app/dashboard/candidate-dashboard/messages/page.tsx
+++ src/app/dashboard/candidate-dashboard/messages/page.tsx
@@ -1,24 +1,14 @@
-'use client'
-import React,{useState} from 'react';
-import Wrapper from '@/layouts/wrapper';
-import CandidateAside from '@/app/components/dashboard/candidate/aside';
+import React from 'react';
+
 import DashboardMessage from '@/app/components/dashboard/candidate/dashboard-message';
 
 const CandidateDashboardMessagesPage = () => {
-  const [isOpenSidebar,setIsOpenSidebar] = useState<boolean>(false);
   return (
-    <Wrapper>
-
-    <div className='main-page-wrapper'>
-      {/* aside start */}
-      <CandidateAside isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar}/>
-      {/* aside end  */}
-
+    <>
       {/* messages area start */}
-      <DashboardMessage setIsOpenSidebar={setIsOpenSidebar}/>
+      <DashboardMessage />
       {/* messages area end */}
-    </div>
-    </Wrapper>
+    </>
   );
 };
 
