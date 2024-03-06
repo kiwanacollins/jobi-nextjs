a03a166973c51387d732ec266e4df585aff71818
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-03-06 22:56:14 +0600

    feat: add google map in contact page

--- src/app/dashboard/admin-dashboard/messages/page.tsx
+++ src/app/dashboard/admin-dashboard/messages/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 import DashboardAdminMessages from '@/app/components/dashboard/admin/DashboardAdminMessages';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 const AdminDashboardMessagesPage = async () => {
+  const user = await currentUser();
+  if (!user || !user.privateMetadata.isAdmin) {
+    return redirect('/');
+  }
   return (
     <>
       {/* messages area start */}
