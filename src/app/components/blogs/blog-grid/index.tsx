bea057736f16461207415b6bcf04ee994bfb70b7
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-03-07 20:49:45 +0600

    Refactor blog-grid component imports and update BlogSidebar props

--- src/app/components/blogs/blog-grid/index.tsx
+++ src/app/components/blogs/blog-grid/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import BlogSidebar from "../blog-postbox/sidebar";
-import BlogPagination from "../blog-postbox/blog-pagination";
-import blog_data from "@/data/blog-data";
-import BlogItem from "../blog-postbox/blog-item";
+import React from 'react';
+import BlogSidebar from '../blog-postbox/sidebar';
+import BlogPagination from '../blog-postbox/blog-pagination';
+import blog_data from '@/data/blog-data';
+import BlogItem from '../blog-postbox/blog-item';
 
 const BlogGridArea = () => {
   const blog_items = blog_data
-    .filter((b) => b.blog === "blog-postbox")
+    .filter((b) => b.blog === 'blog-postbox')
     .slice(0, 6);
   return (
     <section className="blog-section pt-100 lg-pt-80 pb-120 lg-pb-80">
@@ -25,7 +25,7 @@ const BlogGridArea = () => {
 
           <div className="col-lg-4">
             {/* sidebar start */}
-            <BlogSidebar />
+            <BlogSidebar blogs={[]} />
             {/* sidebar  end */}
           </div>
         </div>
