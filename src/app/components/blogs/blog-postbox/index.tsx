711a3c00072357581c9182fd18cb7b3c7f95dc26
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-03-07 20:53:15 +0600

    fix: type error

--- src/app/components/blogs/blog-postbox/index.tsx
+++ src/app/components/blogs/blog-postbox/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import BlogSidebar from "./sidebar";
-import blog_data from "@/data/blog-data";
-import BlogItem from "./blog-item";
-import BlogPagination from "./blog-pagination";
+import React from 'react';
+import BlogSidebar from './sidebar';
+import blog_data from '@/data/blog-data';
+import BlogItem from './blog-item';
+import BlogPagination from './blog-pagination';
 
 const BlogPostboxArea = () => {
   const blog_items = blog_data
-    .filter((b) => b.blog === "blog-postbox")
+    .filter((b) => b.blog === 'blog-postbox')
     .slice(0, 4);
   return (
     <section className="blog-section pt-100 lg-pt-80 pb-120 lg-pb-80">
@@ -24,7 +24,7 @@ const BlogPostboxArea = () => {
 
               <div className="col-lg-4">
                 {/* sidebar start */}
-                <BlogSidebar />
+                <BlogSidebar blogs={[]} />
                 {/* sidebar  end */}
               </div>
             </div>
