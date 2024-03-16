238675b171ff8a0f384eb2fab26261f4f588e86e
Hireskill <visasolutionsbismillah@gmail.com>
2024-03-16 13:10:21 +0600

    feat: fix range error

--- src/app/components/jobs/filter/job-prices.tsx
+++ src/app/components/jobs/filter/job-prices.tsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 import InputRange from '@/ui/input-range';
 import { useRouter, useSearchParams } from 'next/navigation';
-import qs from 'query-string';
-import { formUrlQuery } from '@/utils/utils';
+
 
 // prop type
 type IProps = {
@@ -17,31 +16,13 @@ export function SalaryRangeSlider({
   setPriceValue,
   maxPrice
 }: IProps) {
-  const router = useRouter();
-
-  const searchParams = useSearchParams();
-  const min = searchParams.get('min');
-  const max = searchParams.get('max');
-
-  // eslint-disable-next-line no-unused-vars
-  const [active, setActive] = useState(min || max || '');
+  
   // handleChanges
   const handleChanges = (val: number[]) => {
     setPriceValue(val);
-    const newUrl = qs.stringifyUrl(
-      {
-        url: window?.location?.pathname || '',
-        query: {
-          min: val[0],
-          max: val[1]
-        }
-      },
-      { skipNull: true }
-    );
-
-    router.push(newUrl, { scroll: false });
+    
   };
-
+  
   return (
     <div className="salary-slider">
       <div className="price-input d-flex align-items-center">
@@ -78,38 +59,38 @@ export function SalaryRangeSlider({
 }
 
 const JobPrices = ({ priceValue, setPriceValue, maxPrice }: IProps) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const duration = searchParams.get('duration');
-  const [active, setActive] = useState(duration || '');
+  // const router = useRouter();
+  // const searchParams = useSearchParams();
+  // const duration = searchParams.get('duration');
+  // const [active, setActive] = useState(duration || '');
   const durations = [
     { value: 'Weekly', label: 'Weekly' },
     { value: 'Monthly', label: 'Monthly' },
     { value: 'Hourly', label: 'Hourly' },
     { value: 'Yearly', label: 'Yearly' }
   ];
-  const handleDurationChangle = (item: { value: string; label: string }) => {
-    if (active === item.value) {
-      setActive('');
-      const newUrl = formUrlQuery({
-        params: searchParams.toString(),
-        key: 'duration',
-        value: null
-      });
+  // const handleDurationChangle = (item: { value: string; label: string }) => {
+  //   if (active === item.value) {
+  //     setActive('');
+  //     const newUrl = formUrlQuery({
+  //       params: searchParams.toString(),
+  //       key: 'duration',
+  //       value: null
+  //     });
 
-      router.push(newUrl, { scroll: false });
-    } else {
-      setActive(item.value);
+  //     router.push(newUrl, { scroll: false });
+  //   } else {
+  //     setActive(item.value);
 
-      const newUrl = formUrlQuery({
-        params: searchParams.toString(),
-        key: 'duration',
-        value: item.value.toLowerCase()
-      });
+  //     const newUrl = formUrlQuery({
+  //       params: searchParams.toString(),
+  //       key: 'duration',
+  //       value: item.value.toLowerCase()
+  //     });
 
-      router.push(newUrl, { scroll: false });
-    }
-  };
+  //     router.push(newUrl, { scroll: false });
+  //   }
+  // };
   return (
     <div className="main-body">
       <SalaryRangeSlider
@@ -123,8 +104,8 @@ const JobPrices = ({ priceValue, setPriceValue, maxPrice }: IProps) => {
             <input
               type="radio"
               name="jobDuration"
-              onClick={() => handleDurationChangle(duration)}
-              checked={active === duration.value}
+              // onClick={() => handleDurationChangle(duration)}
+              // checked={active === duration.value}
             />
             <label>{duration.label}</label>
           </li>
