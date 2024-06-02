e4ef666a7dfe28aa0be7242b79b6b7ed1c58e0db
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-06-02 23:57:22 +0600

    refactor: admin route projection

--- src/app/dashboard/admin-dashboard/page.tsx
+++ src/app/dashboard/admin-dashboard/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import DashboardArea from '@/components/dashboard/candidate/dashboard-area';
-import { currentUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
+
 import { getUserStatistics } from '@/lib/actions/admin.action';
 import { getAllCandidates } from '@/lib/actions/candidate.action';
 
 const AdminDashboardPage = async () => {
-  const user = await currentUser();
-  if (!user || !user.privateMetadata.isAdmin) {
-    return redirect('/');
-  }
   const data = await getUserStatistics();
   const { candidates } = await getAllCandidates({});
   return (
